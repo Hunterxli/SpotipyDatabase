67d378ef3bac0c651b957e7ec260b9c110743a42
Hunterxli <846621304@qq.com>
2019-10-24 20:10:46 -0400

    切割出100个user，并把user的歌名抽出来成为新文件

--- devide.py
+++ devide.py
@@ -15,25 +15,75 @@ import matplotlib.pyplot as plt
 #devide the dataset
 data = pd.read_csv('train_triplets.csv', header = None)#'user_id','song_id','listen time'
 data.columns = ['user_id','song_id','listen time']
-user = data.user_id
-#print(data.head())
-user1 = data.user_id[0]#get the first user
+#pd.set_option('max_row', 201)
+#print(data.head(200))
 
-print(user1)
-data1 = data[data.user_id.isin([user1])]
-#print(data1)
-list1 = data1.values.tolist()
-#print(list)
 
-#cut the data base into a smaller one
-my_df = pd.DataFrame(list1)
+# user1 = data.user_id[0]#get the first user
+# print(user1)
+# data1 = data[data.user_id.isin([user1])]
+# #print(data1)
+# list1 = data1.values.tolist()
+# #print(list)
+# #cut the data base into a smaller one
+# my_df = pd.DataFrame(list1)
+# my_df.to_csv("smallexample/user1.csv", index = False, header = False)
+
+# user2 = data.user_id[1]#get the second user
+# data2 = data[data.user_id.isin([user2])]
+# list2 = data2.values.tolist()
+# my_df = pd.DataFrame(list2)
+# my_df.to_csv("smallexample/user2.csv", index = False, header = False)
+
+i = 0
+user_num = 1
+all_user = []
+all_user.append(data.user_id[0])
+#print(data.user_id[0])
+#while(data.user_id[i+1] != ""):
+while(user_num < 100): #finding the first 100 users id
+	if(data.user_id[i+1] != data.user_id[i]):
+		all_user.append(data.user_id[i+1])
+		#print(data.user_id[i+1])
+		user_num = user_num + 1
+		print(user_num)
+		i = i + 1
+	else:
+		i = i + 1
+#print(all_user)
+#print(len(all_user))
+
+t = 1
+the_user = data.user_id[0]
+the_data = data[data.user_id.isin([the_user])]
+the_list = the_data.values.tolist()
+my_df = pd.DataFrame(the_list)
 my_df.to_csv("smallexample/user1.csv", index = False, header = False)
+#while(t < user_num):
+while(t < 100): #taking this 100-user as a example
+	the_user = all_user[t]
+	the_data = data[data.user_id.isin([the_user])]
+	the_list = the_data.values.tolist()
+	my_df = pd.DataFrame(the_list)
+	my_df.to_csv("smallexample/user""%s"".csv" %(t + 1), index = False, header = False)
+	t = t + 1
 
-user2 = data.user_id[1]#get the second user
-data2 = data[data.user_id.isin([user2])]
-list2 = data2.values.tolist()
-my_df = pd.DataFrame(list2)
-my_df.to_csv("smallexample/user2.csv", index = False, header = False)
+
+# #while(t < len(all_user)):
+# while(n < 10):
+	# if(data.user_id[t+1] != data.user_id[t]):
+		# the_user = data.user_id[t]
+		# the_data = data[data.user_id.isin([the_user])]
+		# the_list = the_data.values.tolist()
+		# my_df = pd.DataFrame(the_list)
+		# my_df.to_csv("smallexample/user""%s"".csv" %(n), index = False, header = False)
+		# t = t + 1
+		# n = n + 1
+	# else:
+		# t = t + 1
+		
+	
+	
 
 # csvFile = open("smallexample/data1.csv", "w")
 # writer = csv.writer(csvFile)
