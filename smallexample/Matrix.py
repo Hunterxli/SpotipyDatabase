a83b1f66421f133f15c07e88e6efea12825775db
Hunterxli <846621304@qq.com>
2019-11-20 10:43:54 -0500

    平均数算两首歌相似度

--- smallexample/Matrix.py
+++ smallexample/Matrix.py
@@ -3,9 +3,8 @@ import pandas as pd
 import csv
 import numpy as np
 import pandas as pd
-from decimal import localcontext
 
-def Buildmatrix():
+def Calavg():
 	data = pd.read_csv('keywordcal/allsongcompare.csv')
 	#print(data)
 	#print(data.the_first_song[0])
@@ -16,8 +15,10 @@ def Buildmatrix():
 	word2 = ""
 	result = []
 	calculation = 0
+	avg = 0
+	k = 0
 	#while(i < len(data)):
-	while(i < 587):
+	while(k < 11):
 		song1 = data.the_first_song[i]
 		word1 = data.word1[i]
 		song2 = data.the_second_song[i]
@@ -25,17 +26,22 @@ def Buildmatrix():
 		#print(data.result[i])
 		result.append(data.result[i])
 		#print(result)
-		if(data.the_second_song[i] == data.the_second_song[i + 1]):
-			i = i + 1
-		else:
+		if(data.the_second_song[i] != data.the_second_song[i + 1]):
 			for item in result:
+				#print(item)
 				calculation += float(item)
-		print("{} and {}: {}".format(song1, song2, calculation))
-		calculation = 0
-		print("\n")
+			avg = calculation / (i + 1)
+			#print(avg)
+			print("{} and {}: {}".format(song1, song2, avg))
+			print("\n")
+			k = k + 1
+			calculation = 0
+				
 		i = i + 1
 		
+def BuildMatirx():
+	data = pd.read_csv('keywordcal/allsongcompare.csv')
 	
 	
 
-Buildmatrix()
+Calavg()
